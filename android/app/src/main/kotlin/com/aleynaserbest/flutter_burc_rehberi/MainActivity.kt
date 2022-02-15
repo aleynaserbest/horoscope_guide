588a75605c65ce336356b4e0ee89f82ac4141c81
Aleyna Serbest <aleynaserbestt@outlook.com>
2022-02-15 14:23:24 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/aleynaserbest/flutter_burc_rehberi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aleynaserbest.flutter_burc_rehberi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_burc_rehberi/burc_liste.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Burç Rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.purple[800],
+      ),
+      home:BurcListesi(),
+    );
+  }
+
+}
